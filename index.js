98c27571756237e87b2e82ca3f273eabe8d5e6c9
Tushar Pathak <tusharpathak01110@gmail.com>
2020-03-08 16:31:45 +0530

    initial node app

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    const data = [];
+    res.send("<h1 style='color:red'>hello world</h1>");
+});
+app.get('/current-time', (req, res) => {
+    res.send(`<div style='color:red;text-align: center'>${new Date()}</div>`);
+});
+
+app.get('/about-me', (req, res) => {
+    res.send("<h1 style='color:green'>Hello there, My name is Tushar.</h1>");
+});
+
+app.post('/users', (req, res) => {
+    const users = [
+        {
+            firstName: 'Amit',
+            lastName: 'Dubey',
+            age: 28,
+        },
+        {
+            firstName: 'tushar',
+            lastName: 'pathak',
+            age: 28,
+        },
+        {
+            firstName: 'manish ',
+            lastName: 'Dubey',
+            age: 28,
+        },
+        {
+            firstName: 'Yashu',
+            lastName: 'Dubey',
+            age: 28,
+        }
+    ]
+    res.send(users);
+});
+
+app.listen(port, ()=> console.log(`app running on http://localhost:${port}`));
+
+
